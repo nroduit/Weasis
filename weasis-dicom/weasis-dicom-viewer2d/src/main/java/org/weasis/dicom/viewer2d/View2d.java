1b92505ddead07a9758ffb4f0bd5237e4dc38cb7
Nicolas Roduit <nicolas.roduit@jmicrovision.com>
2014-06-26 10:23:44 +0200

    Fix toggle KO in tiled mode

--- weasis-dicom/weasis-dicom-viewer2d/src/main/java/org/weasis/dicom/viewer2d/View2d.java
+++ weasis-dicom/weasis-dicom-viewer2d/src/main/java/org/weasis/dicom/viewer2d/View2d.java
@@ -324,12 +324,17 @@ public class View2d extends DefaultView2d<DicomImageElement> {
                     actionsInView.put(ActionW.INVERSESTACK.cmd(), val);
                     sortStack(getCurrentSortComparator());
                 } else if (command.equals(ActionW.KO_SELECTION.cmd())) {
+                    int frameIndex =
+                        tile ? JMVUtils.getNULLtoFalse(synch.getView().getActionValue(ActionW.KO_FILTER.cmd())) ? 0
+                            : synch.getView().getFrameIndex() - synch.getView().getTileOffset() : -1;
                     KOManager.updateKOFilter(this, val,
-                        (Boolean) (tile ? synch.getView().getActionValue(ActionW.KO_FILTER.cmd()) : null), tile ? synch
-                            .getView().getFrameIndex() : -1);
+                        (Boolean) (tile ? synch.getView().getActionValue(ActionW.KO_FILTER.cmd()) : null), frameIndex);
                 } else if (command.equals(ActionW.KO_FILTER.cmd())) {
+                    int frameIndex =
+                        tile ? JMVUtils.getNULLtoFalse(val) ? 0 : synch.getView().getFrameIndex()
+                            - synch.getView().getTileOffset() : -1;
                     KOManager.updateKOFilter(this, tile ? synch.getView().getActionValue(ActionW.KO_SELECTION.cmd())
-                        : null, (Boolean) val, tile ? synch.getView().getFrameIndex() : -1);
+                        : null, (Boolean) val, frameIndex);
                 } else if (command.equals(ActionW.CROSSHAIR.cmd())) {
                     if (series != null && val instanceof Point2D.Double) {
                         Point2D.Double p = (Point2D.Double) val;
