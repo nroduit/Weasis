4613e581040927e0e4f6ae130c6728e71a3e800f
Nicolas Roduit <nicolas.roduit@jmicrovision.com>
2015-09-26 17:37:22 +0200

    Share progress in Explorer task

--- weasis-dicom/weasis-dicom-explorer/src/main/java/org/weasis/dicom/explorer/ExplorerTask.java
+++ weasis-dicom/weasis-dicom-explorer/src/main/java/org/weasis/dicom/explorer/ExplorerTask.java
@@ -2,13 +2,21 @@ package org.weasis.dicom.explorer;
 
 import javax.swing.SwingWorker;
 
+import org.weasis.core.api.gui.task.CircularProgressBar;
+
 public abstract class ExplorerTask extends SwingWorker<Boolean, String> {
     private final String message;
     private final boolean interruptible;
+    private final CircularProgressBar bar;
 
     public ExplorerTask(String message, boolean interruptible) {
+        this(message, interruptible, null);
+    }
+
+    public ExplorerTask(String message, boolean interruptible, CircularProgressBar bar) {
         this.message = message;
         this.interruptible = interruptible;
+        this.bar = bar;
     }
 
     public boolean isInterruptible() {
@@ -17,6 +25,10 @@ public abstract class ExplorerTask extends SwingWorker<Boolean, String> {
 
     public String getMessage() {
         return message;
+    }
+
+    public CircularProgressBar getBar() {
+        return bar;
     }
 
 }
