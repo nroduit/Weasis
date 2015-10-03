0614a781e588c7e22d42ce41c72a51fffc4ccbb0
Nicolas Roduit <nicolas.roduit@jmicrovision.com>
2015-10-03 13:34:32 +0200

    Detect CD in sub-directories for Linux

--- weasis-dicom/weasis-dicom-explorer/src/main/java/org/weasis/dicom/explorer/DicomDirImport.java
+++ weasis-dicom/weasis-dicom-explorer/src/main/java/org/weasis/dicom/explorer/DicomDirImport.java
@@ -241,7 +241,7 @@ public class DicomDirImport extends AbstractItemDialogPage implements ImportDico
             LOGGER.error("Cannot import DICOM from {}", file); //$NON-NLS-1$
 
             int response = JOptionPane.showConfirmDialog(this,
-                "No file has been found from DICOMDIR, do you want to import manually?", this.getTitle(),
+                "No new file has been found from DICOMDIR, do you want to import manually?", this.getTitle(),
                 JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
 
             if (response == JOptionPane.YES_OPTION) {
@@ -282,6 +282,10 @@ public class DicomDirImport extends AbstractItemDialogPage implements ImportDico
             } else {
                 dvs.addAll(Arrays.asList(new File("/media").listFiles())); //$NON-NLS-1$
                 dvs.addAll(Arrays.asList(new File("/mnt").listFiles())); //$NON-NLS-1$
+                File userDir = new File("/media/" + System.getProperty("user.name", "local"));
+                if (userDir.exists()) {
+                    dvs.addAll(Arrays.asList(userDir.listFiles()));
+                }
             }
         } catch (Throwable e) {
             LOGGER.error("Error when reading device directories: {}", e.getMessage()); //$NON-NLS-1$
