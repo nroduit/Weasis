48d6cf5b4303d1fafbe060ee000ebaa6b4f46539
Nicolas Roduit <nicolas.roduit@gmail.com>
2016-07-15 16:36:26 +0200

    Tests for graphic serialization

---
+++ weasis-core/weasis-core-ui/src/test/java/org/weasis/core/ui/model/graphic/imp/PointGraphicTest.java
@@ -0,0 +1,75 @@
+package org.weasis.core.ui.model.graphic.imp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+import java.util.List;
+
+import org.weasis.core.api.service.WProperties;
+import org.weasis.core.ui.test.testers.GraphicTester;
+
+public class PointGraphicTest extends GraphicTester<PointGraphic> {
+    private static final String XML_0 = "/graphic/point/point.graphic.0.xml";
+    private static final String XML_1 = "/graphic/point/point.graphic.1.xml";
+    
+    static final String BASIC_TPL = 
+        "<point pointSize=\"%s\" fill=\"%s\" showLabel=\"%s\" thickness=\"%s\" uuid=\"%s\">"
+      +     "<paint rgb=\"%s\"/>"
+      +     "<pts/>"
+      + "</point>";
+    
+    
+    public static final PointGraphic COMPLETE_OBJECT =  new PointGraphic();
+    static {
+        COMPLETE_OBJECT.setUuid(GRAPHIC_UUID_1);
+        COMPLETE_OBJECT.setLineThickness(3.0f);
+        COMPLETE_OBJECT.setColorPaint(Color.RED);
+        
+        List<Point2D.Double> pts = Arrays.asList(new Point2D.Double(1665.5, 987.0));
+        COMPLETE_OBJECT.setPts(pts);
+    }
+    
+    @Override
+    public String getTemplate() {
+        return BASIC_TPL;
+    }
+    
+    @Override
+    public Object[] getParameters() {
+        return new Object[]{
+            PointGraphic.DEFAULT_POINT_SIZE,
+            PointGraphic.DEFAULT_FILLED, 
+            PointGraphic.DEFAULT_LABEL_VISISIBLE,
+            PointGraphic.DEFAULT_LINE_THICKNESS, 
+            getGraphicUuid(), 
+            WProperties.color2Hexadecimal(PointGraphic.DEFAULT_COLOR, true) 
+        };
+    }
+
+    @Override
+    public void additionalTestsForDeserializeBasicGraphic(PointGraphic result, PointGraphic expected) {
+        assertThat(result.getPointSize()).isEqualTo(PointGraphic.DEFAULT_POINT_SIZE);
+    }
+
+    @Override
+    public String getXmlFilePathCase0() {
+        return XML_0;
+    }
+
+    @Override
+    public String getXmlFilePathCase1() {
+        return XML_1;
+    }
+    
+    @Override
+    public PointGraphic getExpectedDeserializeCompleteGraphic() {
+        return COMPLETE_OBJECT;
+    }
+    
+    @Override
+    public void additionalTestsForDeserializeCompleteGraphic(PointGraphic result, PointGraphic expected) {
+        assertThat(result.getPointSize()).isEqualTo(expected.getPointSize());
+    }
+}
---
+++ weasis-core/weasis-core-ui/src/test/java/org/weasis/core/ui/model/graphic/imp/area/EllipseGraphicTest.java
@@ -0,0 +1,67 @@
+package org.weasis.core.ui.model.graphic.imp.area;
+
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+import java.util.List;
+
+import org.weasis.core.api.service.WProperties;
+import org.weasis.core.ui.test.testers.GraphicTester;
+
+public class EllipseGraphicTest extends GraphicTester<EllipseGraphic> {
+    private static final String XML_0 = "/graphic/ellipse/ellipse.graphic.0.xml";
+    private static final String XML_1 = "/graphic/ellipse/ellipse.graphic.1.xml";
+    
+    public static final String BASIC_TPL = 
+        "<ellipse fill=\"%s\" showLabel=\"%s\" thickness=\"%s\" uuid=\"%s\">"
+      +     "<paint rgb=\"%s\"/>"
+      +     "<pts/>"
+      + "</ellipse>";
+    
+    public static final EllipseGraphic COMPLETE_OBJECT =  new EllipseGraphic();
+    static {
+        COMPLETE_OBJECT.setUuid(GRAPHIC_UUID_1);
+        
+        List<Point2D.Double> pts = Arrays.asList(
+            new Point2D.Double(1395.5, 1210.0),
+            new Point2D.Double(2094.5, 1621.0),
+            new Point2D.Double(2094.5, 1210.0),
+            new Point2D.Double(1395.5, 1621.0),
+            new Point2D.Double(1745.0, 1210.0),
+            new Point2D.Double(1745.0, 1621.0),
+            new Point2D.Double(2094.5, 1415.5),
+            new Point2D.Double(1395.5, 1415.5)
+        );
+        COMPLETE_OBJECT.setPts(pts); 
+    }
+    
+    @Override
+    public String getTemplate() {
+        return BASIC_TPL;
+    }
+
+    @Override
+    public Object[] getParameters() {
+        return new Object[]{ 
+            EllipseGraphic.DEFAULT_FILLED, 
+            EllipseGraphic.DEFAULT_LABEL_VISISIBLE,
+            EllipseGraphic.DEFAULT_LINE_THICKNESS,
+            getGraphicUuid(), 
+            WProperties.color2Hexadecimal(EllipseGraphic.DEFAULT_COLOR, true) 
+        };
+    }
+
+    @Override
+    public String getXmlFilePathCase0() {
+        return XML_0;
+    }
+
+    @Override
+    public String getXmlFilePathCase1() {
+        return XML_1;
+    }
+
+    @Override
+    public EllipseGraphic getExpectedDeserializeCompleteGraphic() {
+        return COMPLETE_OBJECT;
+    }
+}
