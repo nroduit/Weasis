e37308c78dfd89c586dba54f26bb99be04c45f0e
Michael Schwarz <mischwarz@users.noreply.github.com>
2016-10-20 12:20:33 +0200

    StreamAdaptor for com.sun.media.imageio.stream.SegmentedImageInputStream when mapper is of type org.dcm4cheri.image.ItemParser

--- weasis-codec/weasis-image-jni/src/main/java/org/weasis/image/jni/StreamSegment.java
+++ weasis-codec/weasis-image-jni/src/main/java/org/weasis/image/jni/StreamSegment.java
@@ -14,7 +14,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.imageio.stream.FileCacheImageInputStream;
@@ -42,7 +44,7 @@ public abstract class StreamSegment {
         if (mlImage == null) {
             return;
         }
-        // Not a good practice (should be instanceof) but necessary to remove the dependency with dcm4che lib
+        // Not a good practice (should be instanceof) but necessary to remove the dependency with dcm4che3 lib
         if ("org.dcm4che3.imageio.stream.SegmentedInputImageStream".equals(iis.getClass().getName())) {
             try {
                 Class<? extends ImageInputStream> clazz = iis.getClass();
@@ -93,35 +95,72 @@ public abstract class StreamSegment {
                         new long[] { 0 }, new int[] { (int) raf.length() }));
             }
         } else if (iis instanceof SegmentedImageInputStream) {
-            throw new IllegalArgumentException("No adaptor implemented yet for SegmentedImageInputStream");
-            // try {
-            // Field f_stream = SegmentedImageInputStream.class.getDeclaredField("stream");
-            // Field f_mapper = SegmentedImageInputStream.class.getDeclaredField("mapper");
-            // if (f_mapper != null && f_stream != null) {
-            // f_mapper.setAccessible(true);
-            // f_stream.setAccessible(true);
-            // FileImageInputStream fstream = (FileImageInputStream) f_stream.get(iis);
-            // Field f_raf = FileImageInputStream.class.getDeclaredField("raf");
-            // if (f_raf != null) {
-            // f_raf.setAccessible(true);
-            // ItemParser mapper = (ItemParser) f_mapper.get(iis);
-            // Field f_parser = ItemParser.class.getDeclaredField("firstItemOfFrame");
-            // if (f_parser != null) {
-            // f_parser.setAccessible(true);
-            // ArrayList<ItemParser.Item> items = (ArrayList<ItemParser.Item>) f_parser.get(mapper);
-            // Item item = items.get(0);
-            //
-            // RandomAccessFile raf = (RandomAccessFile) f_raf.get(fstream);
-            // segment =
-            // new FileStreamSegment(FileStreamSegment.getFileIDfromFileDescriptor(raf.getFD()),
-            // item.startPos, item.length, item.offset);
-            // }
-            // }
-            // }
-            // } catch (Exception e) {
-            // // TODO Auto-generated catch block
-            // e.printStackTrace();
-            // }
+            try {
+                Field f_stream = SegmentedImageInputStream.class.getDeclaredField("stream");
+                Field f_mapper = SegmentedImageInputStream.class.getDeclaredField("mapper");
+                if (f_mapper != null && f_stream != null) {
+                    f_mapper.setAccessible(true);
+                    f_stream.setAccessible(true);
+                    Object mapperObject = f_mapper.get(iis);
+                    // Not a good practice (should be instanceof) but necessary to remove the dependency with dcm4che14 lib
+                    if ("org.dcm4cheri.image.ItemParser".equals(mapperObject.getClass().getName())) {
+                        FileImageInputStream fstream = (FileImageInputStream) f_stream.get(iis);
+                        Field f_raf = FileImageInputStream.class.getDeclaredField("raf");
+                        if (f_raf != null) {
+                            f_raf.setAccessible(true);
+
+                            Field f_items = mapperObject.getClass().getDeclaredField("items");
+                            Method m_numberOfDataFragments = mapperObject.getClass().getMethod("getNumberOfDataFragments");
+                            //Needs to be called so that all segments are added to List "items"
+                            int nbrOfDataFragments = (int) m_numberOfDataFragments.invoke(mapperObject, new Object[]{});
+                            if (f_items != null) {
+                                f_items.setAccessible(true);
+                                ArrayList items = (ArrayList) f_items.get(mapperObject);
+                                if (items.size() > 0) {
+                                    Field f_startPos = items.get(0).getClass().getDeclaredField("startPos");
+                                    Field f_length = items.get(0).getClass().getDeclaredField("length");
+                                    Field f_offset = items.get(0).getClass().getDeclaredField("offset");
+                                    if (f_startPos != null && f_length != null) {
+                                        f_startPos.setAccessible(true);
+                                        f_length.setAccessible(true);
+                                        long[] segmentStartPositions = new long[items.size()];
+                                        int[] segmentLengths = new int[items.size()];
+                                        int startIndex = -1;
+                                        for (int i = 0; i < items.size(); i++) {
+
+                                            if(startIndex == -1 && (long) f_offset.get(items.get(i)) == iis.getStreamPosition()) {
+                                                startIndex = i;
+                                            }
+                                            segmentStartPositions[i] = (long) f_startPos.get(items.get(i));
+                                            segmentLengths[i] = (int) f_length.get(items.get(i));
+                                        }
+                                        if(startIndex != -1) {
+                                            segmentStartPositions = Arrays.copyOfRange(segmentStartPositions, startIndex, segmentStartPositions.length);
+                                            segmentLengths = Arrays.copyOfRange(segmentLengths, startIndex, segmentLengths.length);
+                                            RandomAccessFile raf = (RandomAccessFile) f_raf.get(fstream);
+                                            /*
+                                             * PS 3.5.8.2 Though a fragment may not contain encoded data from more than one frame,
+                                             * the encoded data from one frame may span multiple fragments. See note in Section 8.2.
+                                             */
+                                            FileStreamSegment segment = new FileStreamSegment(raf, FileStreamSegment.getFileIDfromFileDescriptor(raf.getFD()), segmentStartPositions, segmentLengths);
+                                            mlImage.setStreamSegment(segment);
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+                throw new IllegalArgumentException("No adaptor implemented for this type of SegmentedImageInputStream");
+            } catch (Exception e) {
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.error("getFileDescriptor from SegmentedInputImageStream", e);
+                } else {
+                    LOGGER.error(e.getMessage());
+                }
+            }
         } else if (iis instanceof FileCacheImageInputStream) {
             throw new IllegalArgumentException("No adaptor implemented yet for FileCacheImageInputStream");
         } else if (iis instanceof MemoryCacheImageInputStream) {
