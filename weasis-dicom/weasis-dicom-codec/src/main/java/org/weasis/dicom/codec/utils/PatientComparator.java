847a54ac24134b283c99e3c9834b0bb93d916b72
Felipe Fetzer <felipefetzer@animati.com.br>
2017-12-19 13:26:36 -0200

    Just a few modifications

--- weasis-dicom/weasis-dicom-codec/src/main/java/org/weasis/dicom/codec/utils/PatientComparator.java
+++ weasis-dicom/weasis-dicom-codec/src/main/java/org/weasis/dicom/codec/utils/PatientComparator.java
@@ -1,10 +1,4 @@
 /*
- * Copyright (c) 2017 Felipe Fetzer (felipe.fetzer@animati.com.br).
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
  *    Felipe Fetzer (felipe.fetzer@animati.com.br) - initial API and implementation
  */
