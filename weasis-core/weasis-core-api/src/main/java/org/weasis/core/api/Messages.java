9236971b2fdc25a96fbb705cdc6c52d63453398f
Nicolas Roduit <nicolas.roduit@gmail.com>
2018-02-13 18:25:46 +0100

    Update to EPL 2

--- weasis-core/weasis-core-api/src/main/java/org/weasis/core/api/Messages.java
+++ weasis-core/weasis-core-api/src/main/java/org/weasis/core/api/Messages.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2016 Weasis Team and others.
+ * Copyright (c) 2009-2018 Weasis Team and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
- *     ${user} - initial API and implementation
+ *     Nicolas Roduit - initial API and implementation
  *******************************************************************************/
 package org.weasis.core.api;
 
