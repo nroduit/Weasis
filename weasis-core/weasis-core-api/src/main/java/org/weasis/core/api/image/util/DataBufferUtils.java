ee936ba3e4f8dc09b124ef7fdccd6268439d348d
Nicolas Roduit <nicolas.roduit@gmail.com>
2018-02-13 18:43:54 +0100

    Update to EPL 2

--- weasis-core/weasis-core-api/src/main/java/org/weasis/core/api/image/util/DataBufferUtils.java
+++ weasis-core/weasis-core-api/src/main/java/org/weasis/core/api/image/util/DataBufferUtils.java
@@ -1,14 +1,13 @@
-/*
- * $RCSfile: DataBufferUtils.java,v $
+/*******************************************************************************
+ * Copyright (c) 2009-2018 Weasis Team and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
  *
- * Copyright (c) 2005 Sun Microsystems, Inc. All rights reserved.
- *
- * Use is subject to license terms.
- *
- * $Revision: 1.1 $
- * $Date: 2005/02/11 04:57:00 $
- * $State: Exp $
- */
+ * Contributors:
+ *     Nicolas Roduit - initial API and implementation
+ *******************************************************************************/
 package org.weasis.core.api.image.util;
 
 import java.awt.image.DataBuffer;
