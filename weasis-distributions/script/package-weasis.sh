a3a1e2aff75ecd68b8040cc14899d6b55b081607
Nicolas Roduit <nicolas.roduit@gmail.com>
2019-12-20 11:24:22 +0100

    Remove pack200 when linking JDK

--- weasis-distributions/script/package-weasis.sh
+++ weasis-distributions/script/package-weasis.sh
@@ -16,8 +16,7 @@ REVISON_INC="1"
 # jdk.unsupported => sun.misc.Signal
 # jdk.localedata => other locale (en_us) data are included in the jdk.localedata
 # jdk.jdwp.agent => package for debugging agent
-# jdk.pack => jpack required to unpacked bundle.jar.pack.gz
-JDK_MODULES="java.base,java.compiler,java.datatransfer,java.desktop,java.logging,java.management,java.prefs,java.xml,jdk.localedata,jdk.crypto.ec,jdk.crypto.cryptoki,jdk.unsupported,jdk.pack,jdk.jdwp.agent"
+JDK_MODULES="java.base,java.compiler,java.datatransfer,java.desktop,java.logging,java.management,java.prefs,java.xml,jdk.localedata,jdk.crypto.ec,jdk.crypto.cryptoki,jdk.unsupported,jdk.jdwp.agent"
 NAME="Weasis"
 IDENTIFIER="org.weasis.viewer"
 JVM_ARGS="-Dgosh.port=17179 #-Daudit.log=true #-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8789"
