0dbf121e363e31c737300386c044dc3b34585633
Nicolas Roduit <nicolas.roduit@gmail.com>
2022-02-08 20:38:17 +0100

    Fix dynamically change system theme with dark theme (change window border)

--- weasis-launcher/src/main/java/org/weasis/launcher/LookAndFeels.java
+++ weasis-launcher/src/main/java/org/weasis/launcher/LookAndFeels.java
@@ -9,12 +9,9 @@
  */
 package org.weasis.launcher;
 
-import static org.weasis.launcher.WeasisLauncher.P_WEASIS_NAME;
-
 import com.formdev.flatlaf.FlatIconColors;
 import com.formdev.flatlaf.intellijthemes.FlatAllIJThemes;
 import com.formdev.flatlaf.util.ColorFunctions;
-import com.formdev.flatlaf.util.SystemInfo;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,40 +24,51 @@ import javax.swing.UIManager.LookAndFeelInfo;
 public final class LookAndFeels {
   private static final Logger LOGGER = Logger.getLogger(WeasisLauncher.class.getName());
 
-  private LookAndFeels() {}
+  private final List<ReadableLookAndFeelInfo> lookAndFeels;
+  private final ReadableLookAndFeelInfo defaultLightTheme;
+  private final ReadableLookAndFeelInfo defaultDarkTheme;
 
   private static final String[] FLAT_NAMES = {
-    "Core Dark - FlatWeasis",
-    "Core Dark - FlatDarcula",
-    "Core Dark - FlatDark",
-    "Core Light - FlatIntelliJ",
-    "Core Light - FlatLight"
+    "Core Dark - FlatDarcula", "Core Dark - FlatDark", "Core Light - FlatLight"
   };
 
   private static final String[] FLAT_CLASSES = {
-    "org.weasis.launcher.FlatWeasisTheme",
     "com.formdev.flatlaf.FlatDarculaLaf",
     "com.formdev.flatlaf.FlatDarkLaf",
-    "com.formdev.flatlaf.FlatIntelliJLaf",
     "com.formdev.flatlaf.FlatLightLaf"
   };
 
-  public static void installFlatLaf() {
-    List<LookAndFeelInfo> tmp = new ArrayList<>();
+  LookAndFeels() {
+    defaultLightTheme =
+        new ReadableLookAndFeelInfo(
+            "Core Light - FlatIntelliJ", "com.formdev.flatlaf.FlatIntelliJLaf", false);
+    defaultDarkTheme =
+        new ReadableLookAndFeelInfo(
+            "Core Dark - FlatWeasis", "org.weasis.launcher.FlatWeasisTheme", true);
+    lookAndFeels = buildFlatLookAndFeels();
+    lookAndFeels.add(defaultLightTheme);
+    lookAndFeels.add(defaultDarkTheme);
+  }
+
+  private static List<ReadableLookAndFeelInfo> buildFlatLookAndFeels() {
+    List<ReadableLookAndFeelInfo> infos = new ArrayList<>();
     Arrays.asList(FlatAllIJThemes.INFOS)
         .forEach(
             i ->
-                tmp.add(
+                infos.add(
                     new ReadableLookAndFeelInfo(
                         i.isDark() ? "Dark - " + i.getName() : "Light - " + i.getName(),
-                        i.getClassName())));
+                        i.getClassName(),
+                        i.isDark())));
 
     for (int i = 0; i < FLAT_CLASSES.length; i++) {
       if (isClassExist(FLAT_CLASSES[i])) {
-        tmp.add(new ReadableLookAndFeelInfo(FLAT_NAMES[i], FLAT_CLASSES[i]));
+        infos.add(
+            new ReadableLookAndFeelInfo(
+                FLAT_NAMES[i], FLAT_CLASSES[i], FLAT_CLASSES[i].contains("Dark")));
       }
     }
-    UIManager.setInstalledLookAndFeels(tmp.toArray(new LookAndFeelInfo[0]));
+    return infos;
   }
 
   private static boolean isClassExist(String clazz) {
@@ -72,44 +80,33 @@ public final class LookAndFeels {
     }
   }
 
-  /** Changes the look and feel for the whole GUI */
-  public static String setLookAndFeel(String look, String profileName) {
-    String laf = getAvailableLookAndFeel(look, profileName);
-    try {
-      UIManager.setLookAndFeel(laf);
-      for (LookAndFeelInfo lf : UIManager.getInstalledLookAndFeels()) {
-        if (laf.equals(lf.getClassName())) {
-          boolean dark = lf.getName().contains("Dark");
-          increaseToolbarSeparatorContrast(lf, dark);
+  public List<ReadableLookAndFeelInfo> getLookAndFeels() {
+    return lookAndFeels;
+  }
 
-          // TODO set as preference: preserve the default color action
-          applyDefaultColor(FlatIconColors.ACTIONS_RED, dark);
-          applyDefaultColor(FlatIconColors.ACTIONS_YELLOW, dark);
-          applyDefaultColor(FlatIconColors.ACTIONS_GREEN, dark);
-          applyDefaultColor(FlatIconColors.ACTIONS_BLUE, dark);
-          applyDefaultColor(FlatIconColors.ACTIONS_GREY, dark);
-          applyDefaultColor(FlatIconColors.ACTIONS_GREYINLINE, dark);
-          break;
-        }
-      }
+  /** Changes the look and feel for the whole GUI */
+  public String setLookAndFeel(ReadableLookAndFeelInfo look) {
+    ReadableLookAndFeelInfo info = look == null ? defaultDarkTheme : look;
+    boolean dark = info.isDark();
+    try {
+      UIManager.setLookAndFeel(info.getClassName());
+      increaseToolbarSeparatorContrast(dark);
+
+      // TODO set as preference: preserve the default color action
+      applyDefaultColor(FlatIconColors.ACTIONS_RED, dark);
+      applyDefaultColor(FlatIconColors.ACTIONS_YELLOW, dark);
+      applyDefaultColor(FlatIconColors.ACTIONS_GREEN, dark);
+      applyDefaultColor(FlatIconColors.ACTIONS_BLUE, dark);
+      applyDefaultColor(FlatIconColors.ACTIONS_GREY, dark);
+      applyDefaultColor(FlatIconColors.ACTIONS_GREYINLINE, dark);
 
     } catch (Exception e) {
       LOGGER.log(Level.SEVERE, "Unable to set the Look&Feel", e);
     }
-    adaptSystemProperties();
-    return laf;
+    return info.getClassName();
   }
 
-  private static void adaptSystemProperties() {
-    if (SystemInfo.isMacOS) {
-      // Enable screen menu bar
-      System.setProperty("apple.laf.useScreenMenuBar", "true");
-      System.setProperty("apple.awt.application.name", System.getProperty(P_WEASIS_NAME));
-      System.setProperty("apple.awt.application.appearance", "system");
-    }
-  }
-
-  private static void increaseToolbarSeparatorContrast(LookAndFeelInfo lf, boolean dark) {
+  private static void increaseToolbarSeparatorContrast(boolean dark) {
     Color c = UIManager.getColor("ToolBar.separatorColor");
     if (dark) {
       c = ColorFunctions.lighten(c, 0.2f);
@@ -137,31 +134,30 @@ public final class LookAndFeels {
     }
   }
 
-  public static String getAvailableLookAndFeel(String look, String profileName) {
-    UIManager.LookAndFeelInfo[] lafs = UIManager.getInstalledLookAndFeels();
-    String laf = null;
-    if (look != null) {
-      for (UIManager.LookAndFeelInfo lookAndFeelInfo : lafs) {
+  public ReadableLookAndFeelInfo getAvailableLookAndFeel(String look, String profileName) {
+    if (Utils.hasText(look)) {
+      for (ReadableLookAndFeelInfo lookAndFeelInfo : lookAndFeels) {
         if (lookAndFeelInfo.getClassName().equals(look)) {
-          laf = look;
-          break;
+          return lookAndFeelInfo;
         }
       }
     }
-    if (laf == null) {
-      if ("dicomizer".equalsIgnoreCase(profileName)) { // NON-NLS
-        laf = "com.formdev.flatlaf.FlatIntelliJLaf";
-      } else {
-        laf = "org.weasis.launcher.FlatWeasisTheme";
-      }
+    if ("dicomizer".equalsIgnoreCase(profileName)) { // NON-NLS
+      return defaultLightTheme;
     }
-    return laf;
+    return defaultDarkTheme;
   }
 
-  private static class ReadableLookAndFeelInfo extends LookAndFeelInfo {
+  public static class ReadableLookAndFeelInfo extends LookAndFeelInfo {
+    private final boolean dark;
 
-    public ReadableLookAndFeelInfo(String name, String className) {
+    public ReadableLookAndFeelInfo(String name, String className, boolean dark) {
       super(name, className);
+      this.dark = dark;
+    }
+
+    public boolean isDark() {
+      return dark;
     }
 
     @Override
