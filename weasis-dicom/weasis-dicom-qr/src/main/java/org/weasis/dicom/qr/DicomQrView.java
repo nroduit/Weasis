3caafbea94fa21517e6dd56cd1a83f848707fe09
Nicolas Roduit <nicolas.roduit@gmail.com>
2022-06-10 14:34:28 +0200

    Fix change in dicomListener

--- weasis-dicom/weasis-dicom-qr/src/main/java/org/weasis/dicom/qr/DicomQrView.java
+++ weasis-dicom/weasis-dicom-qr/src/main/java/org/weasis/dicom/qr/DicomQrView.java
@@ -18,6 +18,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.FormatStyle;
 import java.time.temporal.TemporalAdjusters;
@@ -332,12 +333,12 @@ public class DicomQrView extends AbstractItemDialogPage implements ImportDicom {
                 DicomModel.LOADING_EXECUTOR.execute(task);
               }
             });
-        dcmListener = new DicomListener(tempDir, progress);
+        dcmListener = new DicomListener(tempDir);
       } else {
         dcmListener = new DicomListener(tempDir);
       }
 
-    } catch (RuntimeException e) {
+    } catch (RuntimeException | IOException e) {
       LOGGER.error("Cannot start DICOM listener", e);
     }
     dicomListener = dcmListener;
