27b9b7e549bc39b35302c5453684eb17e7d671e1
Nicolas Roduit <nicolas.roduit@gmail.com>
2022-06-10 17:23:23 +0200

    Fix empty TextLayout

--- weasis-core/weasis-core-api/src/main/java/org/weasis/core/api/util/FontTools.java
+++ weasis-core/weasis-core-api/src/main/java/org/weasis/core/api/util/FontTools.java
@@ -16,6 +16,7 @@ import java.awt.RenderingHints;
 import java.awt.Shape;
 import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
+import org.weasis.core.util.StringUtil;
 
 public class FontTools {
 
@@ -23,30 +24,33 @@ public class FontTools {
 
   public static void paintColorFontOutline(
       Graphics2D g2, String str, float x, float y, Color color) {
-    g2.setPaint(Color.BLACK);
 
-    if (RenderingHints.VALUE_TEXT_ANTIALIAS_ON.equals(
-        g2.getRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING))) {
-      TextLayout layout = new TextLayout(str, g2.getFont(), g2.getFontRenderContext());
-      AffineTransform textAt = new AffineTransform();
-      textAt.translate(x, y);
-      Shape outline = layout.getOutline(textAt);
-      g2.setStroke(new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND));
-      g2.draw(outline);
-      g2.setPaint(color);
-      g2.setStroke(new BasicStroke(1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND));
-      g2.fill(outline);
-    } else {
-      g2.drawString(str, x - 1f, y - 1f);
-      g2.drawString(str, x - 1f, y);
-      g2.drawString(str, x - 1f, y + 1f);
-      g2.drawString(str, x, y - 1f);
-      g2.drawString(str, x, y + 1f);
-      g2.drawString(str, x + 1f, y - 1f);
-      g2.drawString(str, x + 1f, y);
-      g2.drawString(str, x + 1f, y + 1f);
-      g2.setPaint(color);
-      g2.drawString(str, x, y);
+    if (StringUtil.hasText(str)) {
+      g2.setPaint(Color.BLACK);
+
+      if (RenderingHints.VALUE_TEXT_ANTIALIAS_ON.equals(
+          g2.getRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING))) {
+        TextLayout layout = new TextLayout(str, g2.getFont(), g2.getFontRenderContext());
+        AffineTransform textAt = new AffineTransform();
+        textAt.translate(x, y);
+        Shape outline = layout.getOutline(textAt);
+        g2.setStroke(new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND));
+        g2.draw(outline);
+        g2.setPaint(color);
+        g2.setStroke(new BasicStroke(1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND));
+        g2.fill(outline);
+      } else {
+        g2.drawString(str, x - 1f, y - 1f);
+        g2.drawString(str, x - 1f, y);
+        g2.drawString(str, x - 1f, y + 1f);
+        g2.drawString(str, x, y - 1f);
+        g2.drawString(str, x, y + 1f);
+        g2.drawString(str, x + 1f, y - 1f);
+        g2.drawString(str, x + 1f, y);
+        g2.drawString(str, x + 1f, y + 1f);
+        g2.setPaint(color);
+        g2.drawString(str, x, y);
+      }
     }
   }
 
