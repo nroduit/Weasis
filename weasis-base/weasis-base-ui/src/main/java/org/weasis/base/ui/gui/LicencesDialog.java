bbf408cd908a115cd140017ed8292078c7fe4f2b
Marcelo Riss <marcelo.riss@animati.com.br>
2023-07-12 01:08:01 +0200

    Adding ability to paste and save the license.

--- weasis-base/weasis-base-ui/src/main/java/org/weasis/base/ui/gui/LicencesDialog.java
+++ weasis-base/weasis-base-ui/src/main/java/org/weasis/base/ui/gui/LicencesDialog.java
@@ -1,18 +1,30 @@
 package org.weasis.base.ui.gui;
 
-import org.weasis.base.ui.Messages;
-
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.swing.AbstractAction;
+import javax.swing.ButtonModel;
+import javax.swing.DefaultButtonModel;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
+import javax.swing.text.PlainDocument;
+
+import org.weasis.base.ui.Messages;
 
 /**
  * @author João Bolsson (joaobolsson@animati.com.br)
@@ -22,9 +34,11 @@ import javax.swing.WindowConstants;
 public class LicencesDialog extends JDialog{
 
     private final JTextArea textField;
+    private final JScrollPane textScroll;
     private final JButton btnTest;
     private final JButton okButton;
     private final JButton cancelButton;
+    private LicenseDialogController licenseDialogController;
 
     /**
      * Creates a dialog to insert third party licences.
@@ -35,13 +49,62 @@ public class LicencesDialog extends JDialog{
         super(owner, Messages.getString("LicencesDialog.title"), true);
 
         textField = new JTextArea();
-        textField.setLineWrap(true);
+        textField.setLineWrap(false);
+        textField.setDocument(new PlainDocument());
+
+        textScroll = new JScrollPane(textField);
+
         btnTest = new JButton(Messages.getString("LicencesDialog.btnTest"));
+        DefaultButtonModel newModel = new DefaultButtonModel();
+        newModel.setActionCommand(LicenseDialogController.TEST_COMMAND);
+        newModel.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                licenseDialogController.test();
+            }
+        });
+        btnTest.setModel(newModel);
+
         okButton = new JButton(Messages.getString("LicencesDialog.btnSave"));
+        newModel = new DefaultButtonModel();
+        newModel.setActionCommand(LicenseDialogController.OK_COMMAND);
+        newModel.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                licenseDialogController.save();
+            }
+        });
+        okButton.setModel(newModel);
+
         cancelButton = new JButton(Messages.getString("LicencesDialog.btnCancel"));
+        newModel = new DefaultButtonModel();
+        newModel.setActionCommand(LicenseDialogController.CANCEL_COMMAND);
+        cancelButton.setModel(newModel);
+
+        licenseDialogController = new LicenseDialogController(textField.getDocument(), getButtonModels(), s -> {
+            if (s == LicenseDialogController.STATUS.START_PROCESSING) {
+                textField.setEnabled(false);
+            } else if (s == LicenseDialogController.STATUS.END_PROCESSING) {
+                textField.setEnabled(true);
+            }
+        });
 
         initGUI();
         pack();
+
+    }
+
+    private ButtonModel[] getButtonModels() {
+        Component[] components = this.getComponents();
+        List<ButtonModel> bmList = new ArrayList<ButtonModel>();
+        try (Stream<Component> s = Arrays.asList(components).stream()) {
+            s.forEach( c -> {
+                if (c instanceof JButton) {
+                    bmList.add(((JButton)c).getModel());
+                }
+            });
+        }
+        return bmList.toArray(new ButtonModel[bmList.size()]);
     }
 
     private void initGUI() {
@@ -57,8 +120,8 @@ public class LicencesDialog extends JDialog{
         gbc_textField.fill = GridBagConstraints.HORIZONTAL;
         gbc_textField.gridx = 1;
         gbc_textField.gridy = 0;
-        textField.setPreferredSize(new Dimension(400, 200));
-        add(textField, gbc_textField);
+        textScroll.setPreferredSize(new Dimension(400, 200));
+        add(textScroll, gbc_textField);
 
 
         GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
