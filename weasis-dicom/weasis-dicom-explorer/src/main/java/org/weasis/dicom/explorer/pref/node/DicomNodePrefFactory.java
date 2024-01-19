9901ceb766e5e2cf391e511a55c22512b88e5858
Nicolas Roduit <nicolas.roduit@gmail.com>
2024-01-19 07:48:14 +0100

    Simplify PreferencesPageFactory

--- weasis-dicom/weasis-dicom-explorer/src/main/java/org/weasis/dicom/explorer/pref/node/DicomNodePrefFactory.java
+++ weasis-dicom/weasis-dicom-explorer/src/main/java/org/weasis/dicom/explorer/pref/node/DicomNodePrefFactory.java
@@ -11,7 +11,6 @@ package org.weasis.dicom.explorer.pref.node;
 
 import java.util.Hashtable;
 import org.weasis.core.api.gui.Insertable;
-import org.weasis.core.api.gui.Insertable.Type;
 import org.weasis.core.api.gui.PreferencesPageFactory;
 import org.weasis.core.api.gui.util.AbstractItemDialogPage;
 
@@ -24,15 +23,7 @@ public class DicomNodePrefFactory implements PreferencesPageFactory {
   }
 
   @Override
-  public void dispose(Insertable component) {}
-
-  @Override
   public boolean isComponentCreatedByThisFactory(Insertable component) {
     return component instanceof DicomNodeListView;
-  }
-
-  @Override
-  public Type getType() {
-    return Type.PREFERENCES;
   }
 }
