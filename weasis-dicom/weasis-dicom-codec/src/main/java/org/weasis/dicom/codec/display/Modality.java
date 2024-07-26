4eeb239223f4b17785f5b4b3712015cb29179eba
Nicolas Roduit <nicolas.roduit@gmail.com>
2024-07-26 17:56:36 +0200

    Update modality list

--- weasis-dicom/weasis-dicom-codec/src/main/java/org/weasis/dicom/codec/display/Modality.java
+++ weasis-dicom/weasis-dicom-codec/src/main/java/org/weasis/dicom/codec/display/Modality.java
@@ -15,87 +15,84 @@ import org.weasis.dicom.codec.Messages;
 
 public enum Modality {
   DEFAULT(Messages.getString("Modality.default")),
-
+  ANN("Annotation"), // NON-NLS
+  AR("Autorefraction"), // NON-NLS
+  ASMT("Content Assessment Results"), // NON-NLS
   AU("Audio"), // NON-NLS
-
+  BDUS("Bone Densitometry (ultrasound)"), // NON-NLS
   BI("Biomagnetic imaging"), // NON-NLS
-
-  CD("Color flow Doppler"), // NON-NLS
-
-  DD("Duplex Doppler"), // NON-NLS
-
-  DG("Diaphanography"), // NON-NLS
-
+  BMD("Bone Densitometry (X-Ray)"), // NON-NLS
+  CFM("Confocal Microscopy"), // NON-NLS
   CR("Computed Radiography"), // NON-NLS
-
   CT("Computed Tomography"), // NON-NLS
-
+  CTPROTOCOL("CT Protocol (Performed)"), // NON-NLS
+  DMS("Dermoscopy"), // NON-NLS
+  DG("Diaphanography"), // NON-NLS
+  DOC("Document"), // NON-NLS
   DX("Digital Radiography"), // NON-NLS
-
   ECG("Electrocardiography"), // NON-NLS
-
+  EEG("Electroencephalography"), // NON-NLS
+  EMG("Electromyography"), // NON-NLS
+  EOG("Electrooculography"), // NON-NLS
   EPS("Cardiac Electrophysiology"), // NON-NLS
-
   ES("Endoscopy"), // NON-NLS
-
+  FID("Fiducials"), // NON-NLS
   GM("General Microscopy"), // NON-NLS
-
   HC("Hard Copy"), // NON-NLS
-
   HD("Hemodynamic Waveform"), // NON-NLS
-
-  IO("Intra-oral Radiography"), // NON-NLS
-
+  IO("Intra-Oral Radiography"), // NON-NLS
+  IOL("Intraocular Lens Data"), // NON-NLS
+  IVOCT("Intravascular Optical Coherence Tomography"), // NON-NLS
   IVUS("Intravascular Ultrasound"), // NON-NLS
-
+  KER("Keratometry"), // NON-NLS
+  KO("Key Object Selection"), // NON-NLS
+  LEN("Lensometry"), // NON-NLS
   LS("Laser surface scan"), // NON-NLS
-
   MG("Mammography"), // NON-NLS
-
   MR("Magnetic Resonance"), // NON-NLS
-
+  M3D("Model for 3D Manufacturing"), // NON-NLS
   NM("Nuclear Medicine"), // NON-NLS
-
-  OT("Other"), // NON-NLS
-
+  OAM("Ophthalmic Axial Measurements"), // NON-NLS
+  OCT("Optical Coherence Tomography (non-Ophthalmic)"), // NON-NLS
   OP("Ophthalmic Photography"), // NON-NLS
-
+  OPM("Ophthalmic Mapping"), // NON-NLS
+  OPT("Ophthalmic Tomography"), // NON-NLS
+  OPTBSV("Ophthalmic Tomography B-scan Volume Analysis"), // NON-NLS
+  OPTENF("Ophthalmic Tomography En Face"), // NON-NLS
+  OPV("Ophthalmic Visual Field"), // NON-NLS
+  OSS("Optical Surface Scan"), // NON-NLS
+  OT("Other"), // NON-NLS
+  PA("Photoacoustic"), // NON-NLS
+  PLAN("Plan"), // NON-NLS
+  POS("Position Sensor"), // NON-NLS
   PR("Presentation State"), // NON-NLS
-
-  PX("Panoramic X-Ray"), // NON-NLS
-
   PT("Positron emission tomography (PET)"), // NON-NLS
-
+  PX("Panoramic X-Ray"), // NON-NLS
+  REG("Registration"), // NON-NLS
+  RESP("Respiratory Waveform"), // NON-NLS
   RF("Radio Fluoroscopy"), // NON-NLS
-
   RG("Radiographic imaging (conventional film/screen)"), // NON-NLS
-
   RTDOSE("Radiotherapy Dose"), // NON-NLS
-
   RTIMAGE("Radiotherapy Image"), // NON-NLS
-
+  RTINTENT("Radiotherapy Intent"), // NON-NLS
   RTPLAN("Radiotherapy Plan"), // NON-NLS
-
+  RTRAD("RT Radiation"), // NON-NLS
   RTRECORD("RT Treatment Record"), // NON-NLS
-
+  RTSEGANN("Radiotherapy Segment Annotation"), // NON-NLS
   RTSTRUCT("Radiotherapy Structure Set"), // NON-NLS
-
-  SC("Secondary Capture"), // NON-NLS
-
+  RWV("Real World Value Map"), // NON-NLS
+  SEG("Segmentation"), // NON-NLS
   SM("Slide Microscopy"), // NON-NLS
-
   SMR("Stereometric Relationship"), // NON-NLS
-
   SR("SR Document"), // NON-NLS
-
-  ST("Single-photon emission computed tomography (SPECT)"), // NON-NLS
-
+  SRF("Subjective Refraction"), // NON-NLS
+  STAIN("Automated Slide Stainer"), // NON-NLS
+  TEXTUREMAP("Texture Map"), // NON-NLS
   TG("Thermography"), // NON-NLS
-
   US("Ultrasound"), // NON-NLS
-
+  VA("Visual Acuity"), // NON-NLS
   XA("X-Ray Angiography"), // NON-NLS
-
+  XAPROTOCOL("XA Protocol (Performed)"), // NON-NLS
   XC("External-camera Photography"); // NON-NLS
 
   private final String description;
