36a675acc463742cdf31ef5a34678c0b610d7c5f
Nicolas Roduit <nicolas.roduit@gmail.com>
2025-10-09 18:12:22 +0200

    Improve the performance of pixel info

--- weasis-core/src/main/java/org/weasis/core/ui/editor/image/FocusHandler.java
+++ weasis-core/src/main/java/org/weasis/core/ui/editor/image/FocusHandler.java
@@ -14,20 +14,28 @@ import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.geom.Point2D;
-import java.util.Objects;
 import java.util.Optional;
 import org.opencv.core.Point3;
 import org.weasis.core.Messages;
-import org.weasis.core.api.gui.util.ActionState;
 import org.weasis.core.api.gui.util.Feature;
 import org.weasis.core.api.gui.util.MouseActionAdapter;
 import org.weasis.core.api.gui.util.WinUtil;
 import org.weasis.core.api.media.data.ImageElement;
 import org.weasis.core.ui.model.layer.LayerAnnotation;
+import org.weasis.core.ui.model.layer.LayerItem;
 import org.weasis.core.util.StringUtil;
 
-public class FocusHandler<E extends ImageElement> extends MouseActionAdapter {
+/**
+ * Handles focus and mouse interactions for image viewer canvas. Manages view selection, button
+ * interactions, and pixel information display.
+ *
+ * @param <E> the type of image element
+ */
+public final class FocusHandler<E extends ImageElement> extends MouseActionAdapter {
+  private static final long UPDATE_INTERVAL_MS = 50;
+
   private final ViewCanvas<E> viewCanvas;
+  private long lastUpdateTime;
 
   public FocusHandler(ViewCanvas<E> viewCanvas) {
     this.viewCanvas = viewCanvas;
@@ -35,122 +43,170 @@ public class FocusHandler<E extends ImageElement> extends MouseActionAdapter {
 
   @Override
   public void mousePressed(MouseEvent evt) {
-    ImageViewerPlugin<E> pane = viewCanvas.getEventManager().getSelectedView2dContainer();
-    if (Objects.isNull(pane)) {
+    var pane = viewCanvas.getEventManager().getSelectedView2dContainer();
+    if (pane == null) {
       return;
     }
 
-    ViewButton selectedButton = null;
+    var selectedButton = findViewButtonAt(evt.getPoint());
     // Do select the view when pressing on a view button
-    for (ViewButton b : viewCanvas.getViewButtons()) {
-      if (b.isVisible() && b.contains(evt.getPoint())) {
-        selectedButton = b;
-        break;
-      }
-    }
-
-    if (evt.getClickCount() == 2 && selectedButton == null) {
+    if (evt.getClickCount() == 2 && selectedButton.isEmpty()) {
       pane.maximizedSelectedImagePane(viewCanvas, evt);
       return;
     }
 
-    if (pane.isContainingView(viewCanvas) && pane.getSelectedImagePane() != viewCanvas) {
-      // register all actions of the EventManager with this view waiting the focus gained in some
-      // cases is not
-      // enough, because others mouseListeners are triggered before the focus event (that means
-      // before
-      // registering the view in the EventManager)
-      pane.setSelectedImagePane(viewCanvas);
-    }
-    // request the focus even it is the same pane selected
+    selectViewIfNeeded(pane);
     viewCanvas.getJComponent().requestFocusInWindow();
 
-    // Do select the view when pressing on a view button
-    if (selectedButton != null) {
-      viewCanvas.getJComponent().setCursor(DefaultView2d.DEFAULT_CURSOR);
-      evt.consume();
-      selectedButton.showPopup(evt.getComponent(), evt.getX(), evt.getY());
+    if (selectedButton.isPresent()) {
+      handleViewButtonClick(evt, selectedButton.get());
       return;
     }
 
-    Optional<Feature<? extends ActionState>> action =
-        viewCanvas.getEventManager().getMouseAction(evt.getModifiersEx());
-    viewCanvas
-        .getJComponent()
-        .setCursor(action.isPresent() ? action.get().getCursor() : DefaultView2d.DEFAULT_CURSOR);
+    updateCursorForMouseAction(evt);
   }
 
   @Override
   public void mouseWheelMoved(MouseWheelEvent e) {
     ImageViewerEventManager<E> eventManager = viewCanvas.getEventManager();
-    if (eventManager != null) {
-      ImageViewerPlugin<E> container =
-          WinUtil.getParentOfClass(viewCanvas.getJComponent(), ImageViewerPlugin.class);
-      if (container != null) {
-        if (!container.equals(eventManager.getSelectedView2dContainer())) {
-          eventManager.setSelectedView2dContainer(container);
-        }
-        if (!container.getSelectedImagePane().equals(viewCanvas)) {
-          container.setSelectedImagePane(viewCanvas);
-        }
-      }
+    if (eventManager == null) {
+      return;
+    }
+    ImageViewerPlugin<E> container =
+        WinUtil.getParentOfClass(viewCanvas.getJComponent(), ImageViewerPlugin.class);
+    if (container == null) {
+      return;
+    }
+
+    if (!container.equals(eventManager.getSelectedView2dContainer())) {
+      eventManager.setSelectedView2dContainer(container);
+    }
+    if (!container.getSelectedImagePane().equals(viewCanvas)) {
+      container.setSelectedImagePane(viewCanvas);
     }
   }
 
   @Override
   public void mouseMoved(MouseEvent evt) {
     showPixelInfos(evt);
-    for (ViewButton b : viewCanvas.getViewButtons()) {
-      if (b.isVisible()) {
-        boolean hover = b.contains(evt.getPoint());
-        if (hover != b.isHover()) {
-          b.setHover(hover);
-          viewCanvas.getJComponent().repaint();
-        }
-      }
-    }
+    updateViewButtonsHoverState(evt.getPoint());
   }
 
   @Override
   public void mouseReleased(MouseEvent evt) {
     viewCanvas.getJComponent().setCursor(DefaultView2d.DEFAULT_CURSOR);
-    for (ViewButton b : viewCanvas.getViewButtons()) {
-      if (b.isVisible() && b.contains(evt.getPoint())) {
-        evt.consume();
-        break;
-      }
+    findViewButtonAt(evt.getPoint()).ifPresent(_ -> evt.consume());
+  }
+
+  private Optional<ViewButton> findViewButtonAt(Point point) {
+    return viewCanvas.getViewButtons().stream()
+        .filter(ViewButton::isVisible)
+        .filter(button -> button.contains(point))
+        .findFirst();
+  }
+
+  private void selectViewIfNeeded(ImageViewerPlugin<E> pane) {
+    if (pane.isContainingView(viewCanvas) && pane.getSelectedImagePane() != viewCanvas) {
+      // Register all EventManager actions immediately with this view. Waiting for focus gain
+      // is not enough since other MouseListeners may trigger before the focus event occurs,
+      // resulting in the view not yet being registered in the EventManager.
+      pane.setSelectedImagePane(viewCanvas);
     }
   }
 
-  protected void showPixelInfos(MouseEvent mouseevent) {
-    LayerAnnotation infoLayer = viewCanvas.getInfoLayer();
-    if (infoLayer != null) {
-      Point2D pModel =
-          viewCanvas.getImageCoordinatesFromMouse(mouseevent.getX(), mouseevent.getY());
-      PixelInfo pixelInfo =
-          viewCanvas.getPixelInfo(
-              new Point((int) Math.floor(pModel.getX()), (int) Math.floor(pModel.getY())));
-      if (pixelInfo == null) {
-        return;
-      }
-      Rectangle oldBound = infoLayer.getPixelInfoBound();
+  private void handleViewButtonClick(MouseEvent evt, ViewButton button) {
+    viewCanvas.getJComponent().setCursor(DefaultView2d.DEFAULT_CURSOR);
+    evt.consume(); // Consume event to not select the view
+    button.showPopup(evt.getComponent(), evt.getX(), evt.getY());
+  }
+
+  private void updateCursorForMouseAction(MouseEvent evt) {
+    var action = viewCanvas.getEventManager().getMouseAction(evt.getModifiersEx());
+    var cursor = action.map(Feature::getCursor).orElse(DefaultView2d.DEFAULT_CURSOR);
+    viewCanvas.getJComponent().setCursor(cursor);
+  }
+
+  private void updateViewButtonsHoverState(Point mousePoint) {
+    viewCanvas.getViewButtons().stream()
+        .filter(ViewButton::isVisible)
+        .forEach(
+            button -> {
+              boolean hover = button.contains(mousePoint);
+              if (hover != button.isHover()) {
+                button.setHover(hover);
+                viewCanvas.getJComponent().repaint();
+              }
+            });
+  }
+
+  private void showPixelInfos(MouseEvent mouseevent) {
+    var infoLayer = viewCanvas.getInfoLayer();
+    if (!isPixelInfoVisible(infoLayer)) {
+      return;
+    }
+
+    if (!shouldUpdatePixelInfo()) {
+      return;
+    }
+
+    var pixelInfo = computePixelInfo(mouseevent);
+    if (pixelInfo == null) {
+      return;
+    }
+    updatePixelInfoDisplay(pixelInfo);
+  }
+
+  private boolean isPixelInfoVisible(LayerAnnotation infoLayer) {
+    return infoLayer != null
+        && infoLayer.getVisible()
+        && infoLayer.getDisplayPreferences(LayerItem.PIXEL)
+        && !infoLayer.getDisplayPreferences(LayerItem.MIN_ANNOTATIONS);
+  }
+
+  private boolean shouldUpdatePixelInfo() {
+    long currentTime = System.currentTimeMillis();
+    if (currentTime - lastUpdateTime < UPDATE_INTERVAL_MS) {
+      return false;
+    }
+    lastUpdateTime = currentTime;
+    return true;
+  }
+
+  private PixelInfo computePixelInfo(MouseEvent mouseevent) {
+    Point2D pModel = viewCanvas.getImageCoordinatesFromMouse(mouseevent.getX(), mouseevent.getY());
+    var pixelInfo =
+        viewCanvas.getPixelInfo(
+            new Point((int) Math.floor(pModel.getX()), (int) Math.floor(pModel.getY())));
+    if (pixelInfo != null) {
       Point3 point3d =
           viewCanvas.getVolumeCoordinatesFromMouse(mouseevent.getX(), mouseevent.getY());
       pixelInfo.setPosition3d(point3d);
-      oldBound.width =
-          Math.max(
-              oldBound.width,
-              viewCanvas
-                      .getJComponent()
-                      .getGraphics()
-                      .getFontMetrics()
-                      .stringWidth(
-                          Messages.getString("DefaultView2d.pix")
-                              + StringUtil.COLON_AND_SPACE
-                              + pixelInfo)
-                  + 4);
-      infoLayer.setPixelInfo(pixelInfo);
-      viewCanvas.getJComponent().repaint(oldBound);
     }
+
+    return pixelInfo;
+  }
+
+  private void updatePixelInfoDisplay(PixelInfo pixelInfo) {
+    var infoLayer = viewCanvas.getInfoLayer();
+    Rectangle oldBound = infoLayer.getPixelInfoBound();
+    int textWidth = calculatePixelInfoTextWidth(pixelInfo);
+    oldBound.width = Math.max(oldBound.width, textWidth + 4);
+
+    infoLayer.setPixelInfo(pixelInfo);
+    viewCanvas.getJComponent().repaint(oldBound);
+  }
+
+  private int calculatePixelInfoTextWidth(PixelInfo pixelInfo) {
+    String text =
+        Messages.getString("DefaultView2d.pix")
+            + StringUtil.COLON_AND_SPACE
+            + pixelInfo.getPixelValueText()
+            + " - "
+            + pixelInfo.getPixelPositionText();
+
+    return viewCanvas
+        .getJComponent()
+        .getFontMetrics(viewCanvas.getJComponent().getFont())
+        .stringWidth(text);
   }
 }
